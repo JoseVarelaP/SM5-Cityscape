dcb396d81ec4926f24881588f1660095427b8df2
Jose_Varela <jo.lu6853@gmail.com>
2018-06-04 09:23:52 -0500

    Added initial files

---
+++ BGAnimations/ScreenTitleMenu background/default.lua
@@ -0,0 +1,28 @@
+return Def.ActorFrame{
+	LoadActor("hex1.png")..{ OnCommand=cmd(Center;linear,0.3;glowshift;effectperiod,2;effectmagnitude,0.5,0.5,0.5;); },
+	LoadActor("hex2.png")..{ OnCommand=cmd(Center;y,SCREEN_CENTER_Y+(206-240);linear,0.3;glowshift;effectperiod,2;effectmagnitude,0.5,0.5,0.5;); },
+
+	LoadActor("optionsb.png")..{ OnCommand=cmd(Center;zoom,0.5); },
+	LoadActor("cityscape.png")..{ OnCommand=cmd(xy,SCREEN_CENTER_X+(178-320),SCREEN_CENTER_Y+(235-240);zoom,.4); },
+}
+-- [BGAnimation]
+-- LengthSeconds=25
+
+-- [Layer1]
+-- Import=../_mtrain bg
+
+-- [Layer6]
+-- Type=0		// 0=sprite, 1=stretch, 2=particles, 3=tiles
+-- File=../_black.png
+-- Command=x,320;y,10;diffusealpha,0.5;zoomtowidth,640;zoomtoheight,20
+
+-- [Layer9]
+-- Type=0		// 0=sprite, 1=stretch, 2=particles, 3=tiles
+-- File=cityscape.png
+-- Command=x,178;y,235;zoom,.4;diffusealpha,1.0
+
+-- [Layer10]
+-- Type=0		// 0=sprite, 1=stretch, 2=particles, 3=tiles
+-- File=../border.png
+-- Command=
+
---
+++ BGAnimations/_mtrain bg/default.lua
@@ -0,0 +1,44 @@
+return Def.ActorFrame{
+	
+},
+
+-- [BGAnimation]
+-- LengthSeconds=25
+
+-- [Layer1]
+-- Type=1		// 0=sprite, 1=stretch, 2=particles, 3=tiles
+-- File=bg.png
+-- Command=zoom,1.2;vibrate;effectmagnitude,2,2,2
+
+-- [Layer2]
+-- Type=1		// 0=sprite, 1=stretch, 2=particles, 3=tiles
+-- File=bg.png
+-- Command=zoom,1.2;sleep,0.1;diffusealpha,0.5;vibrate;effectmagnitude,3,3,3
+
+-- [Layer3]
+-- Type=1	// 0=sprite, 1=stretch, 2=particles, 3=tiles
+-- File=bg.png
+-- Command=diffusealpha,0.7
+
+-- [Layer5]
+-- Type=1	// 0=sprite, 1=stretch, 2=particles, 3=tiles
+-- File=bgtop.png
+-- Command=
+
+-- [Layer6]
+-- Type=1		// 0=sprite, 1=stretch, 2=particles, 3=tiles
+-- File=bgtr.png
+-- Command=zoom,0.5;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;x,320;y,240;linear,0.1;x,322;y,241;linear,0.1;
+
+-- [Layer7]
+-- File=../snow.png
+-- Type=2		// 0=sprite, 1=stretch, 2=particles, 3=tiles
+-- Condition=MonthOfYear() == 12 and DayOfMonth() > 22 and DayOfMonth() < 27
+-- ZoomMin=.25
+-- ZoomMax=.5
+-- VelocityXMin=-40
+-- VelocityXMax=40
+-- VelocityYMin=60
+-- VelocityYMax=70
+-- NumParticles=90
+-- Command=diffusealpha,0.7;spin
